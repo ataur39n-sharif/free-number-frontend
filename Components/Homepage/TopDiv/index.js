894e248f9b855456812f0e5097f52113352e0582
ataur39n-sharif <rahman.sharif39n@gmail.com>
2022-08-16 04:00:10 +0600

    top div image modify

--- Components/Homepage/TopDiv/index.js
+++ Components/Homepage/TopDiv/index.js
@@ -1,16 +1,18 @@
+import Image from "next/image"
 import { Col, Row } from "react-bootstrap"
 
 const TopDiv = () => {
     return (
-        <Row className="container text-center m-auto">
-            <Col className="mt-3">
+        // <Row className="container text-center m-auto" style={{height:"100%",width:"100vw"}}>
+            <div >
                 <h1 className="p-2">Receive SMS Online from our Free Virtual Phone Number</h1>
-                <p>
+                <p className="mt-2">
                     We provide free temporary phone numbers to Receive SMS Online. <br />
                     You can Receive SMS Verification code from all the countries and for Facebook, Gmail, WhatsApp, Twitter and more
                 </p>
-            </Col>
-        </Row>
+            </div>
+        // </Row >
+
     )
 }
 
