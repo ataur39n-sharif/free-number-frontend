db944062d589fc60fc3b934648d211a1a2628acc
ataur39n-sharif <rahman.sharif39n@gmail.com>
2022-08-21 03:25:00 +0600

    navbar modify

--- Components/Homepage/Navbar/index.js
+++ Components/Homepage/Navbar/index.js
@@ -3,11 +3,11 @@ import { Container, Nav, Navbar } from "react-bootstrap"
 const NavBar = () => {
     return (
         <Navbar expand={'md'} style={{ minWidth: '18rem', zIndex: "99" }} bg="dark" variant="dark">
-            <Navbar.Brand href="/"><img className="ms-4" src="/image/free-number-logo.png" alt="" style={{height:"38px"}}/></Navbar.Brand>
-            <Container>
+            {/* <Container> */}
                 {/* <Navbar.Brand href="/">Temp-number</Navbar.Brand> */}
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
+                <Navbar.Brand href="/"><img className="ms-4" src="/image/free-number-logo.png" alt="" style={{ height: "38px" }} /></Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" className="m-3"/>
+                <Navbar.Collapse id="responsive-navbar-nav" className="ms-4 me-4">
                     <Nav className="ms-auto">
                         <Nav.Link href="/">Home</Nav.Link>
                         <Nav.Link href="/number-list/us">USA</Nav.Link>
@@ -20,7 +20,7 @@ const NavBar = () => {
                         <Nav.Link href="/contact-us">Contact us</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
-            </Container>
+            {/* </Container> */}
         </Navbar>
     )
 }
