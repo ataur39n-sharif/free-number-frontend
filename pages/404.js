e2b8de30c363f08396bc336fcf1ce4bda1843c39
Ataur Rahman <rahman.sharif39n@gmail.com>
2023-11-27 14:09:54 +0600

    404 page responsive issue fixe

--- pages/404.js
+++ pages/404.js
@@ -3,18 +3,19 @@ import { useEffect } from "react"
 
 const Custom404Page = () => {
     const router = useRouter()
-    useEffect(()=>{
-        setTimeout(()=>{
+    useEffect(() => {
+        setTimeout(() => {
             router.replace('/')
-        },1500)
-    },[])
+        }, 1500)
+    }, [])
     return (
         <div>
-            <section style={{
-                textAlign:'center'
+            <section className="d-flex justify-content-center align-items-center" style={{
+                // textAlign: 'center',
+                height: "90vh",
             }}>
                 <img src="/image/error-page.png" style={{
-                    height:"100vh",
+                    maxWidth: "100vw"
                 }}></img>
             </section>
         </div>
